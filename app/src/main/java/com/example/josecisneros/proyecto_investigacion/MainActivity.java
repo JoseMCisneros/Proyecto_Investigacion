4a921d166da8d5df6b8f00d6cabb1b6074800702
manu58manu <37636700+manu58manu@users.noreply.github.com>
2018-03-21 18:31:00 -0600

    poniendo los botonsitos
    ahuevo, no puedes conmigo pinshi vato loco

--- app/src/main/java/com/example/josecisneros/proyecto_investigacion/MainActivity.java
+++ app/src/main/java/com/example/josecisneros/proyecto_investigacion/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.josecisneros.proyecto_investigacion;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
     Button btnchoose;
     Button btnupload;
-    ImageView imgview;
+    ImageView imgView;
 
     private Uri filePath;
 
@@ -30,7 +43,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
         btnchoose = findViewById(R.id.btnchoose);
         btnupload = findViewById(R.id.btnupload);
-        imgview = findViewById(R.id.imgview);
+        imgView = findViewById(R.id.imgview);
 
         btnchoose.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,10 +65,62 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
     public void chooseImg(){
 
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Seleccionar Imagen"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imgView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void uploadImg(){
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Subiendo...");
+            progressDialog.show();
 
+            StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Subida Exitosa", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Algo ha salido mal "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Progreso "+(int)progress+"%");
+                        }
+                    });
+        }
     }
 
     @Override
